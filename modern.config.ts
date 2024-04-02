212a634d953abae01816deb127e1b268be7ea90e
AllenClark <allen.clark@attuned.ai>
2024-04-02 11:21:35 +0900

    Basic bug replication

--- modern.config.ts
+++ modern.config.ts
@@ -2,12 +2,37 @@ import { appTools, defineConfig } from '@modern-js/app-tools';
 
 // https://modernjs.dev/en/configure/app/usage
 export default defineConfig({
+  output: {
+    cleanDistPath: true,
+    disableSourceMap: false,
+    polyfill: 'off',
+    distPath: {
+      html: '',
+    },
+    copy: [
+      {
+        from: './env/fileToCopy.txt',
+        to: '',
+        // Remove the " " between `@` and `ts-...` to correct the TS exception to fix the type error
+        // @ ts-expect-error
+        transform(content) {
+          return content;
+        },
+      },
+    ],
+  },
+  html: {
+    disableHtmlFolder: true,
+  },
   runtime: {
     router: true,
   },
   plugins: [
     appTools({
-      bundler: 'webpack', // Set to 'experimental-rspack' to enable rspack ⚡️🦀
+      bundler: 'webpack',
     }),
   ],
+  source: {
+    mainEntryName: 'index',
+  },
 });
